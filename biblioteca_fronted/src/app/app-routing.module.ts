f8a5a8ab636ecf263a56ccac31da39d9d595eba6
Gabriel Ramirez <gavrielramirez77@hotmail.com>
2023-02-08 19:38:13 -0500

    fronted

--- biblioteca_fronted/src/app/app-routing.module.ts
+++ biblioteca_fronted/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { LibroComponent } from './libro/libro.component';
+import { leadingComment } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LibroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
